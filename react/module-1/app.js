1cf5b5a8d4c509ca1e843640a6214528b1b5e13c
Meghamala <meghamala1996@gmail.com>
2023-05-01 00:05:40 -0700

    fixing issues in rendering

--- react/module-1/app.js
+++ react/module-1/app.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import {} from 'dotenv/config'
 
-const router = express.Router()
 const app = express()
+const router = express.Router()
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html')
+router.get("/", (req, res) => {
+    res.sendFile('index.html', {root:'public'})
 })
 
 app.use(router)
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
